eecb3afe0d969aabdaf652690edd753a3fd2f7fe
ironshadowz <chenwl911@gmail.com>
2023-09-14 23:49:01 +0800

    done.........

--- NextChapter_back/src/main/java/MiniProject/NextChapter_back/Config/SecurityConfig.java
+++ NextChapter_back/src/main/java/MiniProject/NextChapter_back/Config/SecurityConfig.java
@@ -29,11 +29,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests((requests) -> requests
-                .requestMatchers(HttpMethod.POST, "/authenticate", "/registeruser", "/addLink/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/home/**", "/watchlist/**", "/setting/**", "/weather").permitAll()
-                .requestMatchers(HttpMethod.PUT, "/delete/**", "/updatechapter", "/setting/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/#/**").permitAll()
-                .anyRequest().authenticated());
+                .anyRequest().permitAll());
         return http.build();
     }
 }
